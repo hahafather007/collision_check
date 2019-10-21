56384105ec0042fe8f86efbceed2055d0d62c22e
pangtong <pangtong@98kplay.com>
2019-10-21 15:41:45 +0800

    测试数据

--- test/collision_check_test.dart
+++ test/collision_check_test.dart
@@ -1,74 +1,41 @@
 import 'package:collision_check/src/other/offset.dart';
-import 'package:collision_check/src/shape/circle.dart';
 import 'package:collision_check/src/shape/complex.dart';
-import 'package:collision_check/src/shape/rect.dart';
 import 'package:collision_check/collision_check.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  print("a".codeUnits);
-
   test("碰撞测试", () {
-//    // 矩形与圆形
-//    final value1 = CollisionUtil.isCollision(
-//        CcRect(2, 2), CcCircle(3, center: CcOffset(2, 3)));
-//    assert(value1 == true);
-//
-//    // 圆形与圆形
-//    final value2 = CollisionUtil.isCollision(
-//        CcCircle(1, center: CcOffset(2, 2)),
-//        CcCircle(1, center: CcOffset(2, 4)));
-//    assert(value2 == true);
-//
-//    // 圆形与圆形
-//    final value3 = CollisionUtil.isCollision(
-//        CcCircle(1, center: CcOffset(2, 2)),
-//        CcCircle(1, center: CcOffset(3, 4)));
-//    assert(value3 == false);
-//
-//    // 圆形与圆形
-//    final value4 = CollisionUtil.isCollision(
-//        CcCircle(1, center: CcOffset(2, 2)),
-//        CcCircle(1, center: CcOffset(2, 3)));
-//    assert(value4 == true);
-//
-//    // 复杂几何图形（下面是一个⭐️）与圆形
-//    final complex = CcComplex([
-//      CcOffset(1, 1),
-//      CcOffset(2.8, 1),
-//      CcOffset(3, 0),
-//      CcOffset(3.2, 1),
-//      CcOffset(5, 1),
-//      CcOffset(3.5, 2),
-//      CcOffset(4, 4),
-//      CcOffset(3, 3),
-//      CcOffset(2, 4),
-//      CcOffset(2.5, 2),
-//    ]);
-//    for (var i = 0; i < 1000000; i++) {
-//      complex.position = CcOffset(1, 1);
-//      final value5 = CollisionUtil.isCollision(
-//          complex, CcCircle(2, center: CcOffset(1, 1.2)));
-//      assert(value5 == true);
-//    }
-//
-//    final complex2 = CcComplex([
-//      CcOffset(1, 1),
-//      CcOffset(2.8, 1),
-//      CcOffset(3, 0),
-//      CcOffset(3.2, 1),
-//      CcOffset(5, 1),
-//      CcOffset(3.5, 2),
-//      CcOffset(4, 4),
-//      CcOffset(3, 3),
-//      CcOffset(2, 4),
-//      CcOffset(2.5, 2),
-//    ]);
-//    for (var i = 0; i < 1000000; i++) {
-//      complex.position = CcOffset(1, 1.12312);
-//      complex2.position = CcOffset(4.12343, 0.2384892349);
-//      final value6 = CollisionUtil.isCollision(complex, complex2);
-//      assert(value6 == true);
-//    }
+    // 复杂几何图形（下面是一个⭐️）
+    final complex = CcComplex([
+      CcOffset(25, 0),
+      CcOffset(31, 18),
+      CcOffset(50, 18),
+      CcOffset(34, 31),
+      CcOffset(40, 50),
+      CcOffset(25, 38),
+      CcOffset(10, 50),
+      CcOffset(14, 31),
+      CcOffset(0, 18),
+      CcOffset(18, 18),
+    ]);
+
+    final complex2 = CcComplex([
+      CcOffset(25, 0),
+      CcOffset(31, 18),
+      CcOffset(50, 18),
+      CcOffset(34, 31),
+      CcOffset(40, 50),
+      CcOffset(25, 38),
+      CcOffset(10, 50),
+      CcOffset(14, 31),
+      CcOffset(0, 18),
+      CcOffset(18, 18),
+    ]);
+    for (var i = 0; i < 1000000; i++) {
+      complex.position = CcOffset(1, 1.12312);
+      complex2.position = CcOffset(4.12343, 0.2384892349);
+      final value6 = CollisionUtil.isCollision(complex, complex2);
+      assert(value6 == true);
+    }
   });
 }
